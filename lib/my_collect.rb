6366fb60e9d4bdad681c4d6479835d716262a9e9
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 17:24:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,3 @@
 
 def my_collect(languages)
-  i = 0
-  collection = []
-  while i < languages.length
-    collection << yield languages[i]
-    i += 1
-  end
-  collection
-end
-
-my_collect(languages) do |language|
-  language
 end
